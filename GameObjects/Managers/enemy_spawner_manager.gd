99fbf593d44956a240f98c4fcc9bebcc69bd7984
Yolo-Arts <Jackteong123@gmail.com>
2026-01-05 13:43:51 +0000

    enemy spawner

---
+++ GameObjects/Managers/enemy_spawner_manager.gd
@@ -0,0 +1,37 @@
+extends Node
+
+const SPAWN_RADIUS = 300
+
+@export var basic_enemy_scene: PackedScene 
+
+
+func _on_timer_timeout():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+	
+	var enemy = basic_enemy_scene.instantiate() as Node2D
+	get_parent().add_child(enemy)
+	enemy.global_position = get_spawn_position()
+
+func get_spawn_position():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		print("what")
+		return Vector2.ZERO
+	
+	var spawn_position = Vector2.ZERO
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	for i in 4:
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+		var additional_check_offset = random_direction * 20
+
+		var query_paramaters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position + additional_check_offset, 1 << 3)
+		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_paramaters)
+
+		if result.is_empty():
+			break
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+	
+	return spawn_position
