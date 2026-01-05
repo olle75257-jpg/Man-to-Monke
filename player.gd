b2708581723661b268ad138884216578a580fe7f
Yolo-Arts <Jackteong123@gmail.com>
2026-01-05 12:45:07 +0000

    added camera and camera shake

--- player.gd
+++ player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @export var move_speed: float = 200.0
+@export var acceleration: float = 10
+@export var friction: float = 15
 @export var max_health: int = 5
 var health: int
 @export var fire_delay: float = 0.25
@@ -8,7 +10,7 @@ var fire_timer: float = 0.0
 @export var bullet_scene: PackedScene
 var knockback_velocity: Vector2 = Vector2.ZERO
 @export var knockback_resistance: float = 0.25
-
+@export var recoil_strength: float = 900.0
 
 
 func _ready() -> void:
@@ -31,8 +33,12 @@ func _physics_process(delta: float) -> void:
 
 	if input_vector.length() > 0:
 		input_vector = input_vector.normalized()
-
-	velocity = input_vector * move_speed
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.lerp(input_vector * move_speed, acceleration * delta)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, friction * delta)
+	
 	velocity += knockback_velocity
 	knockback_velocity = knockback_velocity.lerp(Vector2.ZERO, knockback_resistance)	
 	move_and_slide()
@@ -42,10 +48,14 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_pressed("shoot") and fire_timer <= 0.0:
 		shoot()
+		var recoil_dir = (global_position - get_global_mouse_position()).normalized()
+		velocity += recoil_dir * recoil_strength
 		fire_timer = fire_delay
 
 
 func shoot() -> void:
+	Globals.camera.shake(0.25, 10, 15)
+	
 	if bullet_scene == null:
 		return
 	
@@ -60,6 +70,7 @@ func shoot() -> void:
 
 
 func apply_hit(hit_dir: Vector2, damage: int, force: float) -> void:
+	Globals.camera.shake(0.25, 25, 15)
 	health -= damage
 	knockback_velocity += hit_dir * force
 
