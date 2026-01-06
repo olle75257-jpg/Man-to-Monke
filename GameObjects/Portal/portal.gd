c254d89ce3d92648bf01fbb5702d2d83e691a53f
Yolo-Arts <Jackteong123@gmail.com>
2026-01-06 21:26:27 +0000

    scene change between eras

--- GameObjects/Portal/portal.gd
+++ GameObjects/Portal/portal.gd
@@ -13,4 +13,11 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		print("Player entered portal")
+		call_deferred("change_era_scene")
+
+func change_era_scene():
+		match Globals.era:
+			"Modern":
+				get_tree().change_scene_to_file("res://Scenes/medieval.tscn")
+			"Medieval":
+				print("Stone Age scene not built yet")
--- Scenes/medieval.gd
+++ Scenes/medieval.gd
@@ -7,14 +7,14 @@ extends Node2D
 var portal_spawned: bool = false
 
 func _ready() -> void:
-	Globals.era = "Modern"
+	Globals.era = "Medieval"
 	portal_spawned = false
 	Globals.kills = 0
 	Globals.camera = camera
 	Signals.kills_changed.connect(summon_portal)
 
 func summon_portal():
-	if Globals.kills >= 10 && !portal_spawned:
+	if Globals.kills > 10 && !portal_spawned:
 		portal_spawned = true
 		var portal = portal_scene.instantiate() as Node2D
 		portal.global_position = portal_spawn_position.global_position
