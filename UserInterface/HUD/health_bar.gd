11aea16e01f036d9969b05b4b4e72ccb79eef0f7
Yolo-Arts <Jackteong123@gmail.com>
2026-01-09 15:16:38 +0000

    health bar

--- UserInterface/HUD/health_bar.gd
+++ UserInterface/HUD/health_bar.gd
@@ -1,10 +1,12 @@
 extends Control
 @onready var player: CharacterBody2D = %Player
 @onready var label: Label = $Label
+@onready var health_progress_bar: TextureProgressBar = $HealthProgressBar
 
 func _ready() -> void:
 	if player:
 		label.text = str(player.health) + "/" + str(player.max_health) + " HP"
+		health_progress_bar.value = player.health
 
 
 func _on_player_health_changed() -> void:
@@ -12,3 +14,4 @@ func _on_player_health_changed() -> void:
 		return
 	
 	label.text = str(player.health) + "/" + str(player.max_health) + " HP"
+	health_progress_bar.value = player.health
--- UserInterface/HUD/kill_count.gd
+++ UserInterface/HUD/kill_count.gd
@@ -3,8 +3,8 @@ extends Control
 @onready var label: Label = $HBoxContainer/Label
 
 func _ready() -> void:
-	label.text = "Kills: " + str(Globals.kills)
+	label.text = "Kills:  " + str(Globals.kills)
 	Signals.kills_changed.connect(update_text)
 
 func update_text():
-	label.text = "Kills: " + str(Globals.kills)
+	label.text = "Kills:  " + str(Globals.kills)
